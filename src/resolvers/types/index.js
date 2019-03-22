7d9603e738ce897cfc89f0da17b19d2a3f6d8826
Erislandio Soares <erislandiosoares21@gmail.com>
2019-03-21 22:06:06 -0300

    Adição do resolver - schema

---
+++ src/resolvers/types/index.js
@@ -0,0 +1,5 @@
+import { mergeTypes } from 'merge-graphql-schemas'
+import User from './User';
+const typeDefs = [User]
+
+export default mergeTypes(typeDefs, { all: true })
--- src/server.js
+++ src/server.js
@@ -1,1 +1,34 @@
-console.log('teste')
+import express from "express";
+import expressGraphQL from "express-graphql";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import config from '../.config'
+import schema from './graphql';
+
+const user = 'erislandio'
+
+const app = express();
+const PORT = process.env.PORT || "4000";
+const db = `mongodb://@ds161255.mlab.com:61255/eris-teste`;
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    auth: {
+        user,
+        password: config.pass
+    }
+}).then(() => console.log("Banco de dados MLAB está conectado")).catch(err => console.log(err));
+
+
+app.use(
+    "/graphql",
+    cors(),
+    bodyParser.json(),
+    expressGraphQL({
+        schema,
+        graphiql: true
+    })
+);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
